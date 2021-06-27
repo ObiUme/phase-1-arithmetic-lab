4797c79e72e65d9366fd84e0a348a35539852b87
Obi <mr.obiume@gmail.com>
2021-06-26 18:43:36 -0700

    commit tree 541711dbb75d62950c2dcafe2ae46bfef3f45061

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+multiply;
 
-// Write your code here
+let random = Math.random();
+random = (random * 6) + 1;
+random = Math.floor(random);
+random;
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+mod;
+
+let max = Math.max(1, 2, 3, 4, 20);
+max;
